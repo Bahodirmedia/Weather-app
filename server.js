2eb1d716b42fc2be07872afba0246734ac1015e3
Bahodir Abdullayev <60607484+Bahodirmedia@users.noreply.github.com>
2020-11-15 00:11:59 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
+
+// Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+/* Dependencies */
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+/* Initializing the main project folder */
+app.use(express.static('website'));
+
+
+// TODO-Spin up the server
+
+const port = 5000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+	console.log(`running on localhost: ${port}`);
+};
+
+// get route
+app.get('/all', sendData);
+
+function sendData(req, res) {
+	console.log('GET');
+	res.send(projectData);
+};
+
+// post route
+app.post('/addWeather', addWeather)
+
+function addWeather(req, res) {
+	let data = req.body;
+	console.log('Data from server side', data);
+	projectData.date = data.date;
+	projectData.temp = data.temp;
+	projectData.content = data.content;
+	res.send(projectData);
+}
---
+++ website/app.js
@@ -0,0 +1,55 @@
+/* Global Variables */
+const baseURL = "http://api.openweathermap.org/data/2.5/weather?&zip=";
+const apiKey = "&APPID=908e82f11abd0ba527466cb4d4e77de3&units=metric";
+let temp = document.querySelector('#temp');
+let content = document.querySelector('#content');
+let date = document.querySelector('#date');
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+document.querySelector('#generate').addEventListener('click', createData = async () => {
+  const zipCode = document.querySelector('#zip').value;
+  const content = document.querySelector('#feelings').value;
+  const req = await fetch(`${baseURL}${zipCode}${apiKey}`);
+  try {
+    const data = await req.json();
+    data.content = content;
+    data.date = newDate;
+    await postData('/addWeather', data);
+    updateHTML();
+  } catch (error) {
+    console.error("error", error);
+  }
+});
+
+const getData = async (baseURL = '') => {
+  try {
+    const res = await fetch(baseURL);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const postData = async (baseURL = '', data = {}) => {
+  const req = await fetch(baseURL, {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  });
+};
+
+const updateHTML = async () => {
+  const projectData = await getData('/all');
+  console.log(`${projectData}`);
+  date.innerHTML = `${projectData.date}`;
+  temp.innerHTML = `${projectData.temp}`;
+  content.innerHTML = projectData.content;
+};
